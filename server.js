b471d5ee138a4c5d14b7d4b2682b6563d3c4ef10
denohkimkimaiyo <150400638+denohkimkimaiyo@users.noreply.github.com>
2024-01-08 22:49:30 +1100

    final commit

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 const bodyParser = require('body-parser');
 const formController = require('./controllers/formController.js');
 const projectsRouter = require('./routes/projects.js');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static('views'));
 
-app.use(express.static('views')); 
 
 app.post("/submit-form", formController.submitForm);
-app.use('/api/projects', projectsRouter); 
+app.use('/api/projects', projectsRouter);
 
-const port = process.env.PORT || 2022;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+io.on('connection', (socket) => {
+    console.log('A user is connected');
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+
+    setInterval(() => {
+        socket.emit('number', parseInt(Math.random() * 10));
+    }, 5000);
+});
+
+const port = process.env.PORT || 2021;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
